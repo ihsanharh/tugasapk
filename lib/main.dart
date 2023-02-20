57c368c6b2fc5ac0d7dd10d3bf0a0b3be7a5e858
XzFrosT <mschodeihsan@gmail.com>
2023-02-20 21:41:49 +0700

    apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import './pages/navigation.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
