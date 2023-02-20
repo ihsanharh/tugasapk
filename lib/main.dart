e1ecc6f78ebcf47442d6fcc1172a95c01f26572d
XzFrosT <mschodeihsan@gmail.com>
2023-02-20 20:52:24 +0700

    apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:tugasapk/theme.dart';
+import './pages/navigation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,56 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'tugas pr',
-      theme: lightThemeData(context),
+      theme: ThemeData.light(),
       themeMode: ThemeMode.system,
-      darkTheme: darkThemeData(context),
-      home: const MyHomePage(title: 'Tugas PR'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  
-  final String title;
-  
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentPage = 0;
-
-  void _setPage(int pageIndex) {
-    setState(() {
-      _currentPage = pageIndex;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-  	var screen = MediaQuery.of(context).size;
-  	
-    return Scaffold(
-      	body: Stack(
-      		children: <Widget>[
-      			Text('$_currentPage'),
-      		],
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-      	items: <Widget>[
-      		Icon(Icons.home_rounded, size: 30),
-      		Icon(Icons.calendar_month, size: 30),
-      		Icon(Icons.settings, size: 30),
-    		],
-      	index: _currentPage,
-      	color: Colors.grey.shade900,
-      	backgroundColor: Colors.transparent,
-      	onTap: (index) {
-    			_setPage(index);
-    		},
-      	animationDuration: Duration(milliseconds: 100),
-  		),
+      home: const navigation(),
     );
   }
 }
---
+++ lib/pages/meta_pages.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import './home/main.dart';
+
+List<Widget> pages = [
+	Home(),
+	Home(),
+	Home(),
+];
+
+List<Widget> pages_icon = [
+	Icon(Icons.home_rounded, size: 30),
+	Icon(Icons.calendar_month, size: 30),
+	Icon(Icons.settings, size: 30),
+];
