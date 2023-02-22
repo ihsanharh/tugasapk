81d583970be14586f9a31e4b578b2e8616109d9f
XzFrosT <mschodeihsan@gmail.com>
2023-02-22 21:56:32 +0700

    apk

--- lib/pages/meta_pages.dart
+++ lib/pages/meta_pages.dart
@@ -9,7 +9,7 @@ List<Widget> pages = [
 	Settings(),
 ];
 
-List pages_icon(int ActiveIndex) {
+List<Widget> pages_icon(int ActiveIndex) {
 	return <Widget>[
 		Icon(Icons.home_rounded, size: 30, color: (0 == ActiveIndex) ? Colors.lightBlue : Colors.black ),
 		Icon(Icons.calendar_month, size: 30, color: (1 == ActiveIndex) ? Colors.lightBlue : Colors.black),
