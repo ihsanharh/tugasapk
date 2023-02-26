266b46735417ad1b6cafd26cde4bbd366c9b6b5b
XzFrosT <mschodeihsan@gmail.com>
2023-02-26 20:03:03 +0700

    apk

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: const Navigation(),
+      home: Navigation(),
     );
   }
 }
--- lib/navigation/navigation_controller.dart
+++ lib/navigation/navigation_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/state_manager.dart';
 
 class NavigationController extends GetxController {
-	int currentIndex = 0.obs;
+	var currentIndex = 0.obs;
 	
 	void changePage(int pageIndex) {
 		currentIndex = pageIndex;
